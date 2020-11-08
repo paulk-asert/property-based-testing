8befa1b2396c4407e276b811e6bdbbe99b6a38f5
Paul King <paulk@asert.com.au>
2020-11-08 21:14:10 +1000

    add Jqwik SumBiggestPair example

---
+++ subprojects/SumBiggestPairJqwik/src/test/groovy/MathUtilTest.groovy
@@ -0,0 +1,63 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import net.jqwik.api.Arbitraries
+import net.jqwik.api.Arbitrary
+import net.jqwik.api.Disabled
+import net.jqwik.api.ForAll
+import net.jqwik.api.Property
+import net.jqwik.api.Provide
+import net.jqwik.api.constraints.IntRange
+
+import static util.MathUtil.sumBiggestPair
+
+class MathUtilTest {
+    @Property
+    void checkShort(@ForAll Short a, @ForAll Short b, @ForAll Short c) {
+        assert sumBiggestPair(a, b, c) == [a+b, a+c, b+c].max()
+    }
+
+    @Property
+    @Disabled('Enable to explore int overflow scenario')
+    void checkInteger(@ForAll Integer a, @ForAll Integer b, @ForAll Integer c) {
+        assert sumBiggestPair(a, b, c) == [a+b, a+c, b+c].max()
+    }
+
+    @Property
+    void checkIntegerConstrained(@ForAll @IntRange(min = -1_000_000_000, max = 1_000_000_000) Integer a,
+                                 @ForAll @IntRange(min = -1_000_000_000, max = 1_000_000_000) Integer b,
+                                 @ForAll @IntRange(min = -1_000_000_000, max = 1_000_000_000) Integer c) {
+        assert sumBiggestPair(a, b, c) == [a+b, a+c, b+c].max()
+    }
+
+    @Property
+    void checkIntegerConstrainedProvider(@ForAll('halfMax') Integer a,
+                                         @ForAll('halfMax') Integer b,
+                                         @ForAll('halfMax') Integer c) {
+        assert sumBiggestPair(a, b, c) == [a+b, a+c, b+c].max()
+    }
+
+    @Provide
+    Arbitrary<Integer> halfMax() {
+        int halfMax = Integer.MAX_VALUE >> 1
+        return Arbitraries.integers().between(-halfMax, halfMax)
+    }
+
+    @Property
+    void checkIntegerWithLongCalculations(@ForAll Integer a, @ForAll Integer b, @ForAll Integer c) {
+        def (al, bl, cl) = [a.toLong(), b.toLong(), c.toLong()]
+        assert sumBiggestPair(a, b, c) == [al+bl, al+cl, bl+cl].max().toInteger()
+    }
+}
